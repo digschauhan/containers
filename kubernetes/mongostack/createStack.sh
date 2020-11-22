578c663cc89bf9db76e931b27c07805110f29c49
digschauhan <digs.chauhan@gmail.com>
2020-11-22 11:38:04 -0800

    updated files

--- kubernetes/mongostack/createStack.sh
+++ kubernetes/mongostack/createStack.sh
@@ -1,8 +1,10 @@
-kubectl apply -f ./mongosecret.yaml
 kubectl apply -f ./mongoconfig.yaml
+kubectl apply -f ./mongosecret.yaml
+
 
 kubectl apply -f ./mongodbdeploy.yaml
 kubectl apply -f ./mongoexpressdeploy.yaml
+
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/aws/deploy.yaml
 
@@ -12,5 +14,9 @@ kubectl create secret tls tls-secret --key tls.key --cert tls.crt
 
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
+#kubectl apply -f ./default-backend-deployment.yaml
+
 kubectl apply -f ./mongoingress.yaml
+
+eksctl scale nodegroup --cluster=my-cluster --nodes=2 node1
 
